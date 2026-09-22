b61460c36baba10f31294ae41c8465cc1f4b6794
Gadz003 <dulzg003@gmail.com>
2026-09-21 21:48:56 -0300

    chore: adicionado nova funcao get e create

---
+++ mySql.js
@@ -0,0 +1,45 @@
+import mysql from 'mysql2/promise';
+
+const conexao = async () => {
+    const con = await mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: '123456',
+        database: '4info3'
+    });
+    return con;
+
+    };
+
+const getUsuario = async (id = undefined) => {
+    const con = await conexao();
+    let dados;
+
+    if (!id) {        
+        const dados = await con.query('SELECT * FROM usuarios;');
+    }else {
+        const dados = await con.query('SELECT * FROM usuarios WHERE id_usuarios = ?;', [id]);
+    
+    con.close();
+    return dados[0];
+    }
+    
+    
+};
+
+
+const createUsuario = async (nome, email) => {
+    const con = await conexao();
+    await con.query(
+        'INSERT INTO usuarios (nome, email) VALUES (?, ?);', [nome, email]
+    );
+    con.close();
+    return `Usuario ${nome} adicionado ao Sql!`;
+
+};
+
+
+
+console.log(await getUsuario(2));
+console.log(await createUsuario('João', 'joao@example.com'));
